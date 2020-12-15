38a07c1f35dbf511fa727bd893e2acaaf8cc28f6
Ozasnidev <ozasnidev@gmail.com>
2020-12-15 19:00:52 +0100

    07 - Create a file with Saint data. Broze saints included

---
+++ src/data/saints-data.js
@@ -0,0 +1,91 @@
+export const saintsData = [
+    {
+        id: 'caballero-pegaso',
+        constellation: 'Pegaso',
+        rango: 'Bronce',
+        nombre: 'Seiya',
+        ataque: 'Meteoro de Pegaso',
+        nacionalidad: 'Japón'
+    },
+    {
+        id: 'caballero-dragon',
+        constellation: 'Dragón',
+        rango:'Bronce',
+        nombre: 'Shiryu',
+        ataque: 'Dragón ascendente',
+        nacionalidad: 'Japón'
+    },
+    {
+        id: 'caballero-cisne',
+        constellation: 'Cisne',
+        rango:'Bronce',
+        nombre: 'Hyoga',
+        ataque: 'Dragón ascendente',
+        nacionalidad: 'Rusia'
+    },
+    {
+        id: 'caballero-andromeda',
+        constellation: 'Andrómeda',
+        rango:'Bronce',
+        nombre: 'Shun',
+        ataque: 'Tormenta Nebular',
+        nacionalidad: 'Japón'
+    },
+    {
+        id: 'caballero-fenix',
+        constellation: 'Fénix',
+        rango:'Bronce',
+        nombre: 'Ikki',
+        ataque: 'Alas ardientes del Fénix',
+        nacionalidad: 'Japón'
+    },
+    {
+        id: 'caballero-hydra',
+        constellation: 'Hydra',
+        rango:'Bronce',
+        nombre: 'Ichi',
+        ataque: 'Urpas venenosas',
+        nacionalidad: 'Rusia'
+    },
+    {
+        id: 'caballero-hydra',
+        constellation: 'Hydra',
+        rango:'Bronce',
+        nombre: 'Ichi',
+        ataque: 'Urpas venenosas',
+        nacionalidad: 'Japón'
+    },
+    {
+        id: 'caballero-unicornio',
+        constellation: 'Unicornio',
+        rango:'Bronce',
+        nombre: 'Jabú',
+        ataque: 'Galope del Unicornio',
+        nacionalidad: 'Japón'
+    },
+    {
+        id: 'caballero-lobo',
+        constellation: 'Lobo',
+        rango:'Bronce',
+        nombre: 'Nachi',
+        ataque: 'Colmillo del Lobo',
+        nacionalidad: 'Japón'
+    },
+    {
+        id: 'caballero-osamayor',
+        constellation: 'Osa Mayor',
+        rango:'Bronce',
+        nombre: 'Gekki',
+        ataque: 'Abrazo del oso',
+        nacionalidad: 'Japón'
+    },
+    {
+        id: 'caballero-leonmenor',
+        constellation: 'León Menor',
+        rango:'Bronce',
+        nombre: 'Ban',
+        ataque: 'Rugido de fuego',
+        nacionalidad: 'Japón'
+    },
+    
+]
