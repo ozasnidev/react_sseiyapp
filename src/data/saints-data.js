409909656af010346279f50a38f75d3e085e0614
Ozasnidev <ozasnidev@gmail.com>
2020-12-16 18:30:58 +0100

    08 - Clomplete the saints-data field with silver and gold saints info

--- src/data/saints-data.js
+++ src/data/saints-data.js
@@ -20,7 +20,7 @@ export const saintsData = [
         constellation: 'Cisne',
         rango:'Bronce',
         nombre: 'Hyoga',
-        ataque: 'Dragón ascendente',
+        ataque: 'Polvo de diamantes',
         nacionalidad: 'Rusia'
     },
     {
@@ -38,14 +38,6 @@ export const saintsData = [
         nombre: 'Ikki',
         ataque: 'Alas ardientes del Fénix',
         nacionalidad: 'Japón'
-    },
-    {
-        id: 'caballero-hydra',
-        constellation: 'Hydra',
-        rango:'Bronce',
-        nombre: 'Ichi',
-        ataque: 'Urpas venenosas',
-        nacionalidad: 'Rusia'
     },
     {
         id: 'caballero-hydra',
@@ -87,5 +79,220 @@ export const saintsData = [
         ataque: 'Rugido de fuego',
         nacionalidad: 'Japón'
     },
-    
+    {
+        id: 'caballero-cefeo',
+        constellation: 'Cefeo',
+        rango:'Plata',
+        nombre: 'Albiore/Daidalos',
+        ataque: 'Cadena nebular',
+        nacionalidad: 'Argentina'
+    },
+    {
+        id: 'caballero-heracles',
+        constellation: 'Heracles',
+        rango:'Plata',
+        nombre: 'Algethi',
+        ataque: 'Kornephoros',
+        nacionalidad: 'Chipre'
+    },
+    {
+        id: 'caballero-canmayor',
+        constellation: 'Can Mayor',
+        rango:'Plata',
+        nombre: 'Sirius',
+        ataque: ' Great Mountain Smasher',
+        nacionalidad: 'Alemania'
+    },
+    {
+        id: 'caballero-mosca',
+        constellation: 'Mosca',
+        rango:'Plata',
+        nombre: 'Dio',
+        ataque: 'Dead end fly',
+        nacionalidad: 'Mexico'
+    },
+    {
+        id: 'caballero-cetus',
+        constellation: 'Cetus',
+        rango:'Plata',
+        nombre: 'Moses',
+        ataque: 'Cetus spouting bomber',
+        nacionalidad: 'Nueva Zelanda'
+    },
+    {
+        id: 'caballero-centauro',
+        constellation: 'Centauro',
+        rango:'Plata',
+        nombre: 'Babel',
+        ataque: 'Burning sky',
+        nacionalidad: 'Irak'
+    },
+    {
+        id: 'caballero-lagarto',
+        constellation: 'Lagarto',
+        rango:'Plata',
+        nombre: 'Misty',
+        ataque: 'Mavrou Trypa',
+        nacionalidad: 'Francia'
+    },
+    {
+        id: 'caballero-aguila',
+        constellation: 'Águila',
+        rango:'Plata',
+        nombre: 'Marin',
+        ataque: 'Eable Toe Flash',
+        nacionalidad: 'Japón'
+    },
+    {
+        id: 'caballero-ophiucus',
+        constellation: 'Ofiuco',
+        rango:'Plata',
+        nombre: 'Shaina',
+        ataque: 'Thunder claw',
+        nacionalidad: 'Italia'
+    },
+    {
+        id: 'caballero-cuervo',
+        constellation: 'Cuervo',
+        rango:'Plata',
+        nombre: 'Jamian',
+        ataque: 'Black wing shaft',
+        nacionalidad: 'Reino Unido'
+    },
+    {
+        id: 'caballero-auriga',
+        constellation: 'Auriga',
+        rango:'Plata',
+        nombre: 'Capella',
+        ataque: 'Discos de auriga',
+        nacionalidad: 'Grecia'
+    },
+    {
+        id: 'caballero-cerbero',
+        constellation: 'Cerbero',
+        rango:'Plata',
+        nombre: 'Dante',
+        ataque: 'Cadena de Cerbero',
+        nacionalidad: 'Italia'
+    },
+    {
+        id: 'caballero-perseus',
+        constellation: 'Perseus',
+        rango:'Plata',
+        nombre: 'Algol',
+        ataque: 'Medusa shield shy',
+        nacionalidad: 'Arabia Saudita'
+    },
+    {
+        id: 'caballero-sagita',
+        constellation: 'Sagita',
+        rango:'Plata',
+        nombre: 'Ptolemy',
+        ataque: 'Phantom arrow',
+        nacionalidad: 'Libia'
+    },
+    {
+        id: 'caballero-lira',
+        constellation: 'Lira',
+        rango:'Plata',
+        nombre: 'Orpheé',
+        ataque: 'Serenata nocturna mortal',
+        nacionalidad: 'Grecia'
+    },
+    {
+        id: 'caballero-aries',
+        constellation: 'Aries',
+        rango:'Oro',
+        nombre: 'Mu',
+        ataque: 'Starlight extinction',
+        nacionalidad: 'Mú'
+    },
+    {
+        id: 'caballero-tauro',
+        constellation: 'Tauro',
+        rango:'Oro',
+        nombre: 'Aldebarán',
+        ataque: 'Gran cuerno',
+        nacionalidad: 'Brasil'
+    },
+    {
+        id: 'caballero-geminis',
+        constellation: 'Géminis',
+        rango:'Plata',
+        nombre: 'Saga/Kanon',
+        ataque: 'Galaxian explossion',
+        nacionalidad: 'Grecia'
+    },
+    {
+        id: 'caballero-cancer',
+        constellation: 'Cáncer',
+        rango:'Oro',
+        nombre: 'Deathmask',
+        ataque: 'Shikishiki meikai ha',
+        nacionalidad: 'Italia'
+    },
+    {
+        id: 'caballero-leo',
+        constellation: 'Leo',
+        rango:'Oro',
+        nombre: 'Aioria',
+        ataque: 'Lightning volt',
+        nacionalidad: 'Grecia'
+    },
+    {
+        id: 'caballero-virgo',
+        constellation: 'Virgo',
+        rango:'Oro',
+        nombre: 'Shaka',
+        ataque: 'Tesoro del cielo',
+        nacionalidad: 'India'
+    },
+    {
+        id: 'caballero-libra',
+        constellation: 'Libra',
+        rango:'Oro',
+        nombre: 'Dohko',
+        ataque: 'Cién dragones de Rozan',
+        nacionalidad: 'China'
+    },
+    {
+        id: 'caballero-escorpio',
+        constellation: 'Escorpio',
+        rango:'Oro',
+        nombre: 'Milo',
+        ataque: 'Scarlett needle',
+        nacionalidad: 'Grecia'
+    },
+    {
+        id: 'caballero-sagitario',
+        constellation: 'Sagitario',
+        rango:'Oro',
+        nombre: 'Aioros',
+        ataque: 'Gold Athena arrow',
+        nacionalidad: 'Grecia'
+    },
+    {
+        id: 'caballero-capricornio',
+        constellation: 'Capricornio',
+        rango:'Oro',
+        nombre: 'Shura',
+        ataque: 'Excalibur',
+        nacionalidad: 'España'
+    },
+    {
+        id: 'caballero-acuario',
+        constellation: 'Acuario',
+        rango:'Oro',
+        nombre: 'Camus',
+        ataque: 'Ejecución de la Aurora',
+        nacionalidad: 'Francia'
+    },
+    {
+        id: 'caballero-piscis',
+        constellation: 'Piscis',
+        rango:'Oro',
+        nombre: 'Aphrodite',
+        ataque: 'Real bloody rose',
+        nacionalidad: 'Suecia'
+    }
 ]
