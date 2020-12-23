c8e5d9dc5aecd718ac7f499e5fb9c72f362562fc
Ozasnidev <ozasnidev@gmail.com>
2020-12-23 19:08:37 +0100

    12 - Gold saints image changed

--- src/components/saints/saintCard.js
+++ src/components/saints/saintCard.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const SaintCard = (saint) => {
-    const {id, constellation, name, attack, country} = saint;
+    const {id, constellation, name} = saint;
     return (
         
-        <div className="card ms-3" style={{maxWidth: 540 +'px'}}>
+        <div className="card ms-1" style={{maxWidth: 500 +'px'}}>
             <div className="row no-gutters">
                 <div className="col-md-4">
                     <img src={`./assets/${ id }.jpg`} className="card-img" alt={ name } />
@@ -13,8 +13,7 @@ export const SaintCard = (saint) => {
                 <div className="col-md-8">
                     <div className="card-body">
                         <h5 className="card-title">{constellation}</h5>
-                        <p className="card-text">{name} <small className="text-muted">({country})</small></p>
-                        <p className="card-text">{attack}</p>
+                        <p className="card-text">{name} <small className="text-muted"></small></p>
                         <Link to={`./saint/${id}`}>Más info...</Link>
                     </div>
                 </div>
--- src/components/saints/saintsList.js
+++ src/components/saints/saintsList.js
@@ -6,7 +6,7 @@ export const SaintsList = ({ range }) => {
     const saintsFoundedInDataByRange = getSaintsByRange(range);
     return (
         <div className="container">
-            <div className="row row-cols-1 row-cols-md-3 g-2" data-masonry='{"percentPosition": true }'>
+            <div className="row row-cols-1 row-cols-md-3 g-2">
                 {
                     saintsFoundedInDataByRange.map( saint => (
                         <div className="col" key={saint.id}>
